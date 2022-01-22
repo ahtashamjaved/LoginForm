36dd8f71359170e16c422eaace3beaee0f2982e5
ahtashamjaved <30853827+ahtashamjaved@users.noreply.github.com>
2022-01-22 13:30:39 +0100

    Initial commit

---
+++ LoginForm/FormLogin.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LoginForm
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name";
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(99, 57);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(140, 20);
+            this.tbUserName.TabIndex = 1;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(99, 143);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(66, 23);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(99, 107);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(140, 20);
+            this.tbPassword.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(174, 143);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(66, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 212);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLogin";
+            this.Text = "Form Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox tbUserName;
+        public System.Windows.Forms.Button buttonLogin;
+        public System.Windows.Forms.TextBox tbPassword;
+        public System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ LoginForm/FormLogin.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        public string connectionString = "";
+        public string usersTableName = "";
+        public DataSet usersDataSet;
+        public string logedUserName = "";
+        public string logedUserPassword = "";
+        public string logedUserFullName = "";
+        public string fullNameColumn = "Employee_Name";
+        public bool isLoged = false;
+
+        public void Login()
+        {
+            UserLogin(tbUserName.Text, tbPassword.Text, fullNameColumn);
+            if (isLoged)
+            {
+                MessageBox.Show(logedUserFullName + " Loged in Sucessfull");
+                this.Close();
+            }
+            else
+                MessageBox.Show("Sorry username or Password in incorrect!!! Please try again");
+
+        }
+        public void LogOut()
+        {
+            isLoged = false;
+            UpdateUserDetails();
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            LogOut();
+        }
+
+        public void UserLogin(string userName, string password, string fullNameColumn)
+        {
+            try
+            {
+                string query = "Select top 1 * from " + usersTableName + " where userName = '" + userName + "' and password = '" + password + "'";
+                usersDataSet = LoadData(query);
+                if (usersDataSet.Tables.Count > 0)
+                {
+                    if (usersDataSet.Tables[0].Rows.Count > 0)
+                    {
+
+                        logedUserName = usersDataSet.Tables[0].Rows[0]["userName"].ToString();
+                        logedUserPassword = usersDataSet.Tables[0].Rows[0]["password"].ToString();
+                        logedUserFullName = usersDataSet.Tables[0].Rows[0][fullNameColumn].ToString();
+                        isLoged = true;
+
+                    }
+                    else
+                        isLoged = false;
+                }
+                else
+                    isLoged = false;
+                UpdateUserDetails();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        } // End Function UserLogin
+        public void UpdateUserDetails()
+        {
+            if (!isLoged)
+            {
+                logedUserFullName = "";
+                logedUserName = "";
+                logedUserPassword = "";
+            }
+        } // End function UpdateUserDetails()
+        public DataSet LoadData(string query)
+        {
+
+            DataSet dt = new DataSet();
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = query;
+
+
+            SqlDataAdapter objSqlDataAdapter = new SqlDataAdapter(command);
+            objSqlDataAdapter.Fill(dt);
+            connection.Close();
+            return dt;
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+     
+    }
+}
